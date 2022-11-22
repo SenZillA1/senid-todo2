ed3e5a34daf485b2fcebb8c838e0a18a87ea4aa8
Senid <91287731+SenZillA1@users.noreply.github.com>
2022-11-22 20:23:47 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Form from "./components/Form";
+
+function App() {
+    return ( <
+        div className = "wrapper" >
+        <
+        h1 > Todo List < /h1> <
+        form / >
+        <
+        /div>
+    );
+}
+
+export default App;
